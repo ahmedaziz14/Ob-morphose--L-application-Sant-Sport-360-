a5048fe6af756fec164671fc27780a8e7cd81b96
ahmedaziz14 <ahmedazizbrahim2@gmail.com>
2026-04-15 12:25:12 +0100

    Fix: Augmentation de la limite JSON pour le scanner d'images

--- PSC.BACKEND/app.js
+++ PSC.BACKEND/app.js
@@ -14,8 +14,10 @@ const notificationRoutes =require('./routes/notificationRoutes') ;
 const app = express();
 
 // Middlewares globaux
-app.use(cors()); // Autorise le Frontend
-app.use(express.json()); // Permet de lire le JSON dans req.body
+app.use(cors()); 
+app.use(express.json({ limit: '50mb' })); 
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json()); 
 
 // Routes
 app.use('/api/auth', authRoutes);
